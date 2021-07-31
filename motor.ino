658dca9ed05b2c1eb02dd2864d7b244e6ce6224d
srivenkateshwar <70425638+Srivenkateshwar@users.noreply.github.com>
2021-07-31 16:46:20 +0530

    Add files via upload

---
+++ motor.ino
@@ -0,0 +1,23 @@
+#include <AFMotor.h>
+AF_DCMotor M1(2);
+AF_DCMotor M2(3);
+
+void setup() {
+  M1.setSpeed(150);
+  M2.setSpeed(250);
+}
+
+void loop() {
+  M1.run(FORWARD);
+  M2.run(FORWARD);
+  delay(1000);
+  M1.run(RELEASE);
+  M2.run(RELEASE);
+  delay(1000);
+  M1.run(FORWARD);
+  M2.run(BACKWARD);
+  delay(1000);
+  M1.run(RELEASE);
+  M2.run(RELEASE);
+  delay(1000);
+}
